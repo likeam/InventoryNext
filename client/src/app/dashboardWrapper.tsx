977051ad269dce793d8fd2b5bb76ae1360b6803b
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-03-24 07:59:56 -0700

    Building Dashboard UI

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,17 +1,35 @@
+"use client";
+
 import React from "react";
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSiderbarOpen = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
   return (
     <div className={` flex bg-gray-800 text-gray-50 w-full min-h-screen`}>
-      sidebar
+      <Sidebar />
       <main
-        className={` flex flex-col w-full h-full py-7 px-9 bg-gray-700 md:pl-24`}
+        className={` flex flex-col w-full h-full py-7 px-9 bg-gray-700 ${
+          isSiderbarOpen ? "md:pl-24" : "md:pl-72"
+        }`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
